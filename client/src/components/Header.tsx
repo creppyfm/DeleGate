efe63457f8b21101c6f5e50568f0bde2c94d3ec3
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-04 22:01:30 -0500

    Done with Header styling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,19 @@
 import "./Header.module.css";
-import { Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container, Button } from "react-bootstrap";
 
 export function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="#home">DeleGate</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+          <Button variant="outline-info">Login</Button>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
