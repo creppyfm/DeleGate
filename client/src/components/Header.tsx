0317bfa59bac6ec582e65387b81470e039bbd5fe
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-06 22:05:12 -0500

    Adjusted padding in Header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,12 +6,16 @@ export function Header() {
     <header className={styles.header}>
       <Navbar bg="dark" variant="dark">
         <Container fluid>
-          <Navbar.Brand href="#home">DeleGate</Navbar.Brand>
+          <Navbar.Brand className="ms-2" href="#home">
+            DeleGate
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
           </Nav>
-          <Button variant="outline-info">Login</Button>
+          <Button className="me-2" variant="outline-info">
+            Login
+          </Button>
         </Container>
       </Navbar>
     </header>
