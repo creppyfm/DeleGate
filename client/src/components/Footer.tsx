b3e94096e234cf6f0b1ecfc4f16d9a9ec4df3f05
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-07 11:39:27 -0500

    Added Footer component

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,42 @@
+import { Container, Row, Col, Nav } from "react-bootstrap";
+
+export function Footer() {
+  return (
+    <footer className="d-flex justify-content-start align-items-center pb-3 mb-4 w-100">
+      M
+      <div className="ms-5">
+        <a
+          href="#"
+          className="mb-3 mb-md-0 text-body-secondary text-decoration-none lh-1"
+        >
+          <svg
+            className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="25"
+            height="25"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: 25x25"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#eee" />
+          </svg>
+        </a>
+        <span className="ms-2 mb-3 mb-md-0 text-light">© 2023 Foli Creppy</span>
+      </div>
+      <Nav as="ul">
+        <Nav.Item as="li">
+          <Nav.Link href="https://github.com/creppyfm">
+            <img src="/GitHub.svg" alt="GitHub Badge" />
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link href="https://www.linkedin.com/in/creppyfm/">
+            <img src="/LinkedIn.svg" alt="LinkedIn Badge" />
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </footer>
+  );
+}
