99b2ed8e9ffe74ac195d2631697c04e930aaeb9f
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-11 15:08:44 -0500

    Removed commented code

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -9,39 +9,3 @@ export function LoginPage() {
     </main>
   );
 }
-{
-  /* <form action="/users/login" method="POST">
-        <img
-          class="mb-4 rounded-3"
-          src="/CarLogoDark.png"
-          alt="Carpooly Logo"
-          width="150"
-          height="auto"
-        />
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-        <div class="form-floating input-top">
-          <input
-            type="email"
-            class="form-control"
-            id="email"
-            name="email"
-            placeholder="name@example.com"
-          />
-          <label for="email">Email address</label>
-        </div>
-        <div class="form-floating input-bottom mb-4">
-          <input
-            type="password"
-            class="form-control"
-            id="password"
-            name="password"
-            placeholder="Password"
-          />
-          <label for="password">Password</label>
-        </div>
-
-        <button class="w-100 btn btn-lg btn-primary" type="submit">
-          Sign in
-        </button>
-      </form> */
-}
