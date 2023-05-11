b28ea79dd8b7b2e8c16ce2e35a8bb673f365866d
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-11 15:07:54 -0500

    Login rough draft done

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,3 +1,47 @@
+import { Button } from "react-bootstrap";
+import styles from "./LoginPage.module.css";
 export function LoginPage() {
-  return <div className="text-bg-primary">LoginPage</div>;
+  return (
+    <main className="d-flex flex-column align-items-center gap-3">
+      <h2 className="mb-3 fw-normal text-light">Please Sign In</h2>
+      <Button className={styles["sso-button"]}>Github</Button>
+      <Button className={styles["sso-button"]}>Google</Button>
+    </main>
+  );
+}
+{
+  /* <form action="/users/login" method="POST">
+        <img
+          class="mb-4 rounded-3"
+          src="/CarLogoDark.png"
+          alt="Carpooly Logo"
+          width="150"
+          height="auto"
+        />
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <div class="form-floating input-top">
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            name="email"
+            placeholder="name@example.com"
+          />
+          <label for="email">Email address</label>
+        </div>
+        <div class="form-floating input-bottom mb-4">
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            name="password"
+            placeholder="Password"
+          />
+          <label for="password">Password</label>
+        </div>
+
+        <button class="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+      </form> */
 }
