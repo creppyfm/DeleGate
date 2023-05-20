0e5b4acca2da5fb995209f5b607d4abb40090d49
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-19 21:17:55 -0500

    Added OAuth2 Logins
    Modified the buttons to point to correct endpoints in the API to login.
    
    Implemented the second portion of the OAuth2 flow, which is sending the
    recieved auth code to the backend to exchange for a token.

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,11 +1,70 @@
+import { useEffect } from "react";
 import { Button } from "react-bootstrap";
+import { redirect } from "react-router-dom";
 import styles from "./LoginPage.module.css";
-export function LoginPage() {
+import { useParams } from "react-router-dom";
+import { SetUser } from "../App";
+
+type LoginPageProps = {
+  setUser: SetUser;
+  provider?: string;
+};
+
+export function LoginPage({ setUser, provider }: LoginPageProps) {
+  const { code } = useParams();
+
+  async function getUser() {
+    try {
+      const body = new URLSearchParams({ code: code as string });
+
+      const response = await fetch(`/auth/${provider}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: body,
+      });
+
+      if (!response.ok) {
+        throw new Error("HTTP status " + response.status);
+      }
+
+      const data = await response.json();
+
+      return data.user;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    if (provider) {
+      getUser().then((user) => {
+        setUser({ ...user, loggedIn: true });
+        redirect("/");
+      });
+    }
+  }, []);
+
   return (
     <main className="d-flex flex-column align-items-center gap-3">
       <h2 className="mb-3 fw-normal text-light">Please Sign In</h2>
-      <Button className={styles["sso-button"]}>Github</Button>
-      <Button className={styles["sso-button"]}>Google</Button>
+      <Button
+        className={styles["sso-button"]}
+        onClick={() => {
+          redirect("/auth/github");
+        }}
+      >
+        Github
+      </Button>
+      <Button
+        className={styles["sso-button"]}
+        onClick={() => {
+          redirect("/auth/google");
+        }}
+      >
+        Google
+      </Button>
     </main>
   );
 }
