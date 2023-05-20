e2aa43ec12e80a2542ac1b8957ddce6c64379f7f
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-19 21:14:58 -0500

    Added User Object and Login Routing
    Created state for storing the user. Properties subject to change.
    
    Nested routes inside of /login to determine which provider was used.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,50 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { LoginPage } from "./pages/LoginPage";
 import { AboutUsPage } from "./pages/AboutUsPage";
+import { useState } from "react";
+
+export type User = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  loggedIn: boolean;
+};
+
+export type SetUser = React.Dispatch<
+  React.SetStateAction<{
+    firstName: string;
+    lastName: string;
+    email: string;
+    sessionId: string;
+    loggedIn: boolean;
+  }>
+>;
 
 function App() {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    sessionId: "",
+    loggedIn: false,
+  });
+
   return (
     <>
-      <Header />
+      <Header user={user} setUser={setUser} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login">
+          <Route index element={<LoginPage setUser={setUser} />} />
+          <Route
+            path="github"
+            element={<LoginPage setUser={setUser} provider="github" />}
+          />
+          <Route
+            path="google"
+            element={<LoginPage setUser={setUser} provider="google" />}
+          />
+        </Route>
         <Route path="/about-us" element={<AboutUsPage />} />
       </Routes>
       <Footer />
