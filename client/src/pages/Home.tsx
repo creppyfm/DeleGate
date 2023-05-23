b2b7626e82a1157a1ca74f9c16d789e0f1c99d96
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-22 22:00:05 -0500

    Added check for user data

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,27 @@
 import { Hero } from "../components/Hero";
 import { FeaturetteDivider } from "../components/FeaturetteDivider";
 import { Main } from "../components/Main";
+import { useAppContext } from "../utils/SessionContext";
+import { useEffect } from "react";
 
 export function Home() {
+  const { user, setUser } = useAppContext();
+
+  async function getUserDataIfExists() {
+    const response = await fetch("/user"); // attempt to find user data
+    if (response.ok) {
+      // if user found (becuase session in httpOnly cookie) then process JSON
+      const data = await response.json();
+      setUser({ ...data, loggedIn: true }); // finally, log in user
+    }
+  }
+
+  useEffect(() => {
+    if (!user.loggedIn) {
+      getUserDataIfExists();
+    }
+  }, []);
+
   return (
     <>
       <Hero />
