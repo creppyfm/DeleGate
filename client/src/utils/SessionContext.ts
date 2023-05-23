d78ac870b7bf3360b748f3fcb362698b6c286178
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-22 21:53:14 -0500

    Created default value for useAppContext hook

--- client/src/utils/SessionContext.ts
+++ client/src/utils/SessionContext.ts
@@ -1,7 +1,15 @@
 import { useContext, createContext } from "react";
 import { AppContextProps } from "../App";
 
-export const AppContext = createContext<AppContextProps | null>(null);
+export const AppContext = createContext<AppContextProps>({
+  user: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    loggedIn: false,
+  },
+  setUser: () => {},
+});
 
 export function useAppContext() {
   return useContext(AppContext);
