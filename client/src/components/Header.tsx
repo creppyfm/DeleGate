23abf86fc02b865b6b81fef2ceb3407c15279a1e
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-24 09:39:31 -0500

    Removed unused import

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Nav, Navbar, Container, Button } from "react-bootstrap";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 import { useAppContext } from "../utils/SessionContext";
 
