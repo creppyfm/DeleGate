180a8231f1f418e2466b46c9bf8e78563be0e298
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-25 09:41:52 -0500

    Renamed page

--- client/src/pages/DashboardPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function DashboardPage() {
-  return (
-    <p className="text-light">
-      Some text<i className="bi-alarm text-success"></i>
-    </p>
-  );
-}
