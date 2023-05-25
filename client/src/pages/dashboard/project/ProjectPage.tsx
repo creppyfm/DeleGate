926bff9dc9b0fe1bab2c43e5efc24b919fac6fc3
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-25 13:46:36 -0500

    Created template for the Project view

---
+++ client/src/pages/dashboard/project/ProjectPage.tsx
@@ -0,0 +1,10 @@
+import { Container } from "react-bootstrap";
+
+export function ProjectPage() {
+  return (
+    <Container className="text-light">
+      <h1>Project Page</h1>
+      <div>some text</div>
+    </Container>
+  );
+}
