eb60654690cca83ede723c1d166749cd9206f6e0
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-26 10:54:52 -0500

    Simulated passing props into ProjectCards

--- client/src/pages/dashboard/default/DashboardPage.tsx
+++ client/src/pages/dashboard/default/DashboardPage.tsx
@@ -7,9 +7,20 @@ export function DashboardPage() {
   return (
     <Container className="mt-3">
       <Row xs={1} md={2} xl={3} className="g-4">
-        {Array.from({ length: 4 }).map((_) => (
-          <ProjectCard key={uuidv4()} />
-        ))}
+        {Array.from({ length: 4 }).map((_) => {
+          const randomKey = uuidv4();
+          const project = {
+            projectId: randomKey.slice(randomKey.length / 1.5),
+            title: "Project: " + randomKey.slice(randomKey.length / 1.5),
+            subtitle: null,
+            description:
+              "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            phase: "Not Started",
+            projectMembers: ["Bill", "Joanne"],
+            updated: "November 5, 1605",
+          };
+          return <ProjectCard key={randomKey} project={project} />;
+        })}
       </Row>
     </Container>
   );
