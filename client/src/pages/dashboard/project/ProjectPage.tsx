8195446bfb6ca82b3d8e53262644b81c0a6e4d2b
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-26 10:56:04 -0500

    Building out sample project view

--- client/src/pages/dashboard/project/ProjectPage.tsx
+++ client/src/pages/dashboard/project/ProjectPage.tsx
@@ -1,10 +1,18 @@
-import { Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 
 export function ProjectPage() {
   return (
-    <Container className="text-light">
-      <h1>Project Page</h1>
-      <div>some text</div>
+    <Container>
+      <Card bg="light" className="rounded-4">
+        <Card.Header as="h3">This is the card header!</Card.Header>
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </Container>
   );
 }
