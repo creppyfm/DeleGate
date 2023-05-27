584e250ac1f45d7be9df3049c80388aad9da395a
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-05-27 16:23:24 -0400

    Added CRUD endpoints to StepController. Modified StepService to accommodate minor changes.

--- server/src/main/java/com/creppyfm/server/controller/StepController.java
+++ server/src/main/java/com/creppyfm/server/controller/StepController.java
@@ -1,17 +1,78 @@
 package com.creppyfm.server.controller;
 import com.creppyfm.server.model.Project;
+import com.creppyfm.server.model.Step;
+import com.creppyfm.server.service.ProjectService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/step")
 @CrossOrigin
+@Tag(name = "Step Controller")
 public class StepController {
 
+    @Autowired
+    private StepService stepService;
+    @Autowired
+    private ProjectService projectService;
 
+    @GetMapping("/{projectId}/{id}")
+    public ResponseEntity<Step> getStepById(@PathVariable String id) {
+        Step step = stepService.getStepById(id);
+        if (step != null) {
+            return new ResponseEntity<>(step, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @GetMapping("/{projectId}")
+    public ResponseEntity<List<Step>> getStepsByProjectId(@PathVariable String projectId) {
+        Project project = projectService.findProjectById(projectId);
+        if (project != null) {
+            return new ResponseEntity<>(project.getStepList(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/{projectId}")
+    public ResponseEntity<Step> createStep(@PathVariable String projectId, @RequestBody Step step) {
+        Project project = projectService.findProjectById(projectId);
+        if (project != null) {
+            Step createdStep = stepService.createStep(projectId, step.getTitle(), step.getDescription());
+            project.getStepList().add(createdStep);
+            projectService.updateProject(projectId, project);
+            return new ResponseEntity<>(createdStep, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Step> updateStep(@PathVariable("id") String id, @RequestBody Step updatedStep) {
+        Step step = stepService.updateStep(id, updatedStep);
+        if (step != null) {
+            return new ResponseEntity<>(step, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStep(@PathVariable("id") String id) {
+        boolean isDeleted = stepService.deleteStep(id);
+        if (isDeleted) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
 /*
     @PostMapping("/{id}/tasks/delegate")
