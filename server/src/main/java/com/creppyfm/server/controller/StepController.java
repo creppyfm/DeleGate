17bd1101dba6b6682ff48ec79c056918d555c0fa
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-05-26 23:33:10 -0400

    Removed AllArgs/NoArgs annotation from StepController. Project builds.

--- server/src/main/java/com/creppyfm/server/controller/StepController.java
+++ server/src/main/java/com/creppyfm/server/controller/StepController.java
@@ -1,13 +1,10 @@
 package com.creppyfm.server.controller;
-
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/step")
-@AllArgsConstructor
-@NoArgsConstructor
+@CrossOrigin
 public class StepController {
 }
