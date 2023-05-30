c202407893678489729772fa1653c5889317708b
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-05-30 17:40:05 -0400

    Modified the prompt to request that the openai response format be JSON, rather than raw text, for consistency. Modified the processing method to accommodate the change.

--- server/src/main/java/com/creppyfm/server/data_transfer_object_model/StepDataTransferObject.java
+++ server/src/main/java/com/creppyfm/server/data_transfer_object_model/StepDataTransferObject.java
@@ -1,8 +1,10 @@
 package com.creppyfm.server.data_transfer_object_model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class StepDataTransferObject {
     private String title;
     private String description;
