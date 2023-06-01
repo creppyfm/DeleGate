ed0747318672e80829ce0358c38035d7508c13fd
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-06-01 13:14:42 -0400

    HOT FIX - Modified Project controller, service, and repository to only fetch projects from authenticated user.

--- server/src/main/java/com/creppyfm/server/service/ProjectService.java
+++ server/src/main/java/com/creppyfm/server/service/ProjectService.java
@@ -33,8 +33,8 @@ public class ProjectService {
     @Autowired
     private StepRepository stepRepository;
 
-    public List<ProjectResponse> findAllProjects() {
-        List<Project> projects = projectRepository.findAll();
+    public List<ProjectResponse> findAllProjects(String userId) {
+        List<Project> projects = projectRepository.findAllByUserId(userId);
         List<ProjectResponse> projectResponses = new ArrayList<>();
 
         for (Project project : projects) {
