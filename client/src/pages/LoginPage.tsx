6ed6c815af0a1ec50c5a19b206c160aa4d6d8936
Kenson Johnson <kenson.johnson@outlook.com>
2023-06-01 08:41:43 -0500

    Restyled Login Page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,26 +1,38 @@
-import { Button } from "react-bootstrap";
+import { Button, Card, Image } from "react-bootstrap";
 import styles from "./LoginPage.module.css";
 import { useGetUserDataIfExists } from "../utils/GetUserData";
 
 export function LoginPage() {
   useGetUserDataIfExists();
   return (
-    <main className="d-flex flex-column align-items-center gap-3">
-      <h2 className="mb-3 fw-normal text-light">Please Sign In</h2>
-      <Button
-        className={styles["sso-button"]}
-        as="a"
-        href="/oauth2/authorization/github"
-      >
-        Github
-      </Button>
-      <Button
-        className={styles["sso-button"]}
-        as="a"
-        href="/oauth2/authorization/google"
-      >
-        Google
-      </Button>
-    </main>
+    <Card as="main" bg="dark" className="m-auto">
+      <Card.Img variant="top" src="logo_with_brand_dark_trimmed.png" />
+      <Card.Body>
+        <Card.Title className="text-light text-center">
+          Please Sign In
+        </Card.Title>
+        <div className="d-flex justify-content-around mt-4">
+          <Button
+            className={`fs-4 ${styles["sso-button"]}`}
+            as="a"
+            href="/oauth2/authorization/github"
+          >
+            <i className="bi bi-github" style={{ color: "#181717" }}></i> Github
+          </Button>
+          <Button
+            className={`fs-4 ${styles["sso-button"]}`}
+            as="a"
+            href="/oauth2/authorization/google"
+          >
+            <i className="bi bi-google" style={{ color: "#4285F4" }}></i> Google
+          </Button>
+        </div>
+        <div className="d-flex justify-content-around">
+          <Button className="fs-4 my-4" style={{ width: "20rem" }} disabled>
+            <i className="bi bi-envelope"></i> Log In With Email
+          </Button>
+        </div>
+      </Card.Body>
+    </Card>
   );
 }
