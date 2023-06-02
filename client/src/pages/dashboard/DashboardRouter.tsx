59e90731a66af2cf116779b27c0d2584ac0e6cee
Kenson Johnson <kenson.johnson@outlook.com>
2023-06-01 21:16:02 -0500

    Added new routes

--- client/src/pages/dashboard/DashboardRouter.tsx
+++ client/src/pages/dashboard/DashboardRouter.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { DashboardPage } from "./default/DashboardPage";
 import { ProjectPage } from "./project/ProjectPage";
+import { TaskPage } from "./task/TaskPage";
+import { StepPage } from "../step/StepPage";
 
 export function DashboardRouter() {
   return (
     <Routes>
       <Route path="/" element={<DashboardPage />} />
       <Route path="/project/:id" element={<ProjectPage />} />
+      <Route path="/step/:id" element={<StepPage />} />
+      <Route path="/task/:id" element={<TaskPage />} />
     </Routes>
   );
 }
