a2b1a0ef546497a6581af292616a05d34f874996
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-06-02 23:18:27 -0400

    Added optional conditioning.

--- server/src/main/java/com/creppyfm/server/service/StepService.java
+++ server/src/main/java/com/creppyfm/server/service/StepService.java
@@ -124,67 +124,70 @@ public class StepService {
     }
 
     public void generateTasksForStep(String id) throws IOException {
-        Step step = stepRepository.findStepById(id);
+        Optional<Step> optionalStep = stepRepository.findById(id);
 
-        String stepInfo = String.format("Title: %s\nDescription: %s\n\nList of tasks to complete the step:", step.getTitle(), step.getDescription());
-        String prompt =
-                "Read the title and description of the Step included below, and generate a list " +
-                        "of no more than 10 tasks to complete the Step. The format of your response should be a JSON array, where each " +
-                        "element is an array containing two strings - the title of the task as the first element, and a 3 to 5 sentence description " +
-                        "of the task as the second element. Each task should be detailed and actionable. " +
-                        "The format of each task should match the following example: \n" +
-                        "[\"Database Setup\", \"Download and install MySQL database server from the official website. After installation, " +
-                        "secure the database server by running the security script that comes with the installation. Then, create a new database " +
-                        "for the application and an account that can access and modify it. Finally, test the connection to the database from your " +
-                        "application code to ensure everything is set up correctly.\"]\n" +
-                        "An example of the complete response format is: \n" +
-                        "[\n" +
-                        "[\"Task one title\", \"Task one description\"],\n" +
-                        "[\"Task two title\", \"Task two description\"],\n" +
-                        "[\"Task three title\", \"Task three description\"]\n" +
-                        "...\n" +
-                        "].\n" +
-                        "NOTE: Do not include any extra words, phrases, or sentences unrelated to the tasks you are generating." +
-                        "Do not include phrases such as \"Sure, I can do that,\" or any phrases throughout " +
-                        "or ending your response. Do not include numbering for the tasks. Do not include a comma after " +
-                        "the final sub array (the final task.) ONLY return the list of generated tasks " +
-                        "in the format requested above.\n" +
-                        "Here is the Step information:\n"
-                        + stepInfo;
+        if (optionalStep.isPresent()) {
+            Step step = optionalStep.get();
+            String stepInfo = String.format("Title: %s\nDescription: %s\n\nList of tasks to complete the step:", step.getTitle(), step.getDescription());
+            String prompt =
+                    "Read the title and description of the Step included below, and generate a list " +
+                            "of no more than 10 tasks to complete the Step. The format of your response should be a JSON array, where each " +
+                            "element is an array containing two strings - the title of the task as the first element, and a 3 to 5 sentence description " +
+                            "of the task as the second element. Each task should be detailed and actionable. " +
+                            "The format of each task should match the following example: \n" +
+                            "[\"Database Setup\", \"Download and install MySQL database server from the official website. After installation, " +
+                            "secure the database server by running the security script that comes with the installation. Then, create a new database " +
+                            "for the application and an account that can access and modify it. Finally, test the connection to the database from your " +
+                            "application code to ensure everything is set up correctly.\"]\n" +
+                            "An example of the complete response format is: \n" +
+                            "[\n" +
+                            "[\"Task one title\", \"Task one description\"],\n" +
+                            "[\"Task two title\", \"Task two description\"],\n" +
+                            "[\"Task three title\", \"Task three description\"]\n" +
+                            "...\n" +
+                            "].\n" +
+                            "NOTE: Do not include any extra words, phrases, or sentences unrelated to the tasks you are generating." +
+                            "Do not include phrases such as \"Sure, I can do that,\" or any phrases throughout " +
+                            "or ending your response. Do not include numbering for the tasks. Do not include a comma after " +
+                            "the final sub array (the final task.) ONLY return the list of generated tasks " +
+                            "in the format requested above.\n" +
+                            "Here is the Step information:\n"
+                            + stepInfo;
 
-        OpenAIChatAPIManager openAIChatAPIManager = new OpenAIChatAPIManager();
-        List<List<String>> tasks;
-        try {
-            tasks = openAIChatAPIManager.buildsTaskList(prompt);
-        } catch (IOException | InterruptedException e) {
-            throw new IOException(e);
-        }
-
-        Project project = projectRepository.findByStepListContaining(step);
-        List<Task> taskList = new ArrayList<>();
-        List<String> taskIds = new ArrayList<>();
-        for (List<String> list : tasks) {
-            if (list.size() == 2) {
-                String taskTitle = list.get(0);
-                String taskDescription = list.get(1);
-                Task task = taskService.createTask(id, taskTitle, taskDescription, 0, Phase.NOT_STARTED);
-                taskList.add(task);
-                taskIds.add(task.getId());
+            OpenAIChatAPIManager openAIChatAPIManager = new OpenAIChatAPIManager();
+            List<List<String>> tasks;
+            try {
+                tasks = openAIChatAPIManager.buildsTaskList(prompt);
+            } catch (IOException | InterruptedException e) {
+                throw new IOException(e);
             }
-        }
-        //add tasks to project's taskList
-        if (project.getTaskList() != null) {
-            project.getTaskList().addAll(taskList);
-        } else {
-            project.setTaskList(taskList);
-        }
 
-        if (step.getTaskList() != null) {
-            step.getTaskList().addAll(taskIds);
-        } else {
-            step.setTaskList(taskIds);
+            Project project = projectRepository.findByStepListContaining(step);
+            List<Task> taskList = new ArrayList<>();
+            List<String> taskIds = new ArrayList<>();
+            for (List<String> list : tasks) {
+                if (list.size() == 2) {
+                    String taskTitle = list.get(0);
+                    String taskDescription = list.get(1);
+                    Task task = taskService.createTask(id, taskTitle, taskDescription, 0, Phase.NOT_STARTED);
+                    taskList.add(task);
+                    taskIds.add(task.getId());
+                }
+            }
+            //add tasks to project's taskList
+            if (project.getTaskList() != null) {
+                project.getTaskList().addAll(taskList);
+            } else {
+                project.setTaskList(taskList);
+            }
+
+            if (step.getTaskList() != null) {
+                step.getTaskList().addAll(taskIds);
+            } else {
+                step.setTaskList(taskIds);
+            }
+            stepRepository.save(step);
         }
-        stepRepository.save(step);
     }
 
 
