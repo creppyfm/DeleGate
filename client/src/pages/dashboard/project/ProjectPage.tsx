685e613c25688ea7de50c4438f65fa55e0ec374b
Kenson Johnson <kenson.johnson@outlook.com>
2023-06-03 23:09:15 -0500

    Changed Row to use automatic columns

--- client/src/pages/dashboard/project/ProjectPage.tsx
+++ client/src/pages/dashboard/project/ProjectPage.tsx
@@ -1,11 +1,20 @@
 import { useState, useEffect } from "react";
-import { Card, Col, Container, ListGroup, Row, Spinner } from "react-bootstrap";
+import {
+  Card,
+  Col,
+  Container,
+  ListGroup,
+  Row,
+  Spinner,
+  Stack,
+} from "react-bootstrap";
 import { NavLink, useParams } from "react-router-dom";
 import {
   Project,
   Step,
   useProjectContext,
 } from "../../../utils/GetProjectData";
+import styles from "../default/Dashboard.module.css";
 
 export function ProjectPage() {
   const [loading, setLoading] = useState(true);
@@ -34,55 +43,74 @@ export function ProjectPage() {
   }, [project]);
 
   return (
-    <Container className="pt-5">
+    <Container
+      className={`p-0 overflow-hidden my-0 ${styles["dynamic-height"]}`}
+    >
       <h1 className="text-center text-light mb-5">
         {project && !loading ? project.title : "Loading Project..."}
       </h1>
-      <Row style={{ height: "60%" }}>
-        <Col lg={12} xl={6}>
-          <Card bg="light" className="rounded-4 mb-3">
-            <Card.Header as="h3">Summary</Card.Header>
-            <Card.Body>
-              <Card.Title></Card.Title>
-              <Card.Text>
-                {project && !loading
-                  ? project.description
-                  : "Loading Project..."}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <div className="d-flex justify-content-between">
-                <span>Phase: {project && !loading && project.phase}</span>
-                <span>
-                  Updated:{" "}
-                  {project &&
-                    !loading &&
-                    new Date(project.updated).toISOString()}
-                </span>
-              </div>
-            </Card.Footer>
-          </Card>
-          <ListGroup className="rounded-4">
-            {project && !loading ? (
-              project.stepList.map((step) => {
-                const { id, title }: Step = step;
-                return (
-                  <ListGroup.Item key={id}>
-                    <NavLink to={`/dashboard/step/${id}`}>{title}</NavLink>
-                  </ListGroup.Item>
-                );
-              })
-            ) : (
-              <ListGroup.Item>
-                <div className="d-flex text-warning">
-                  <Spinner animation="border" role="status" className="me-3" />
-                  <h3>Loading... </h3>
+
+      <Row lg={1} xl={2} style={{ height: "inherit" }}>
+        <Col className={`overflow-auto ${styles["card-height"]}`}>
+          <Stack direction="vertical" className="pb-5" gap={3}>
+            <Card bg="light" className="rounded-4">
+              <Card.Header as="h3">Summary</Card.Header>
+              <Card.Body>
+                <Card.Title></Card.Title>
+                <Card.Text>
+                  {project && !loading
+                    ? project.description
+                    : "Loading Project..."}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <div className="d-flex justify-content-between">
+                  <span>Phase: {project && !loading && project.phase}</span>
+                  <span>
+                    Updated:{" "}
+                    {project &&
+                      !loading &&
+                      new Date(project.updated).toISOString()}
+                  </span>
                 </div>
-              </ListGroup.Item>
-            )}
-          </ListGroup>
+              </Card.Footer>
+            </Card>
+
+            <ListGroup variant="flush" className="rounded-4">
+              {project && !loading ? (
+                project.stepList.map((step) => {
+                  const { id, title }: Step = step;
+                  return (
+                    <ListGroup.Item key={id}>
+                      <NavLink to={`/dashboard/step/${id}`}>{title}</NavLink>
+                    </ListGroup.Item>
+                  );
+                })
+              ) : (
+                <ListGroup.Item>
+                  <div className="d-flex text-warning">
+                    <Spinner
+                      animation="border"
+                      role="status"
+                      className="me-3"
+                    />
+                    <h3>Loading... </h3>
+                  </div>
+                </ListGroup.Item>
+              )}
+            </ListGroup>
+          </Stack>
         </Col>
-        <Col lg={12} xl={6}></Col>
+        <Col className={styles["card-height"]}>
+          <Card style={{ height: "inherit" }}>
+            <Card.Header as="h3">Tasks</Card.Header>
+            <ListGroup variant="flush" className="overflow-auto">
+              {project?.taskList.map((task) => {
+                return <ListGroup.Item action>{task.title}</ListGroup.Item>;
+              })}
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </Container>
   );
