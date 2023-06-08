a6f3740688b50230b53eeb30a9f6d344ad64deff
Kenson Johnson <kenson.johnson@outlook.com>
2023-06-08 10:53:56 -0500

    Created skeleton for task page

--- client/src/pages/dashboard/project/step/task/TaskPage.tsx
+++ client/src/pages/dashboard/project/step/task/TaskPage.tsx
@@ -1,3 +1,20 @@
+import { useParams } from "react-router-dom";
+import { useProjectContext } from "../../../../../utils/GetProjectData";
+import { Card, Container } from "react-bootstrap";
+
 export function TaskPage() {
-  return <div className="text-light">TaskPage</div>;
+  const { id } = useParams();
+  const { project } = useProjectContext();
+
+  const fetchedTask = project?.taskList.find((task) => task.id === id);
+
+  return (
+    <Card>
+      <Card.Header>Task Assistant</Card.Header>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>Card Text</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
