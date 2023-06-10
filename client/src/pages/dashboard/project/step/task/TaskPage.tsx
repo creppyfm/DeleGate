a7308c256df99fe5893b9194a510e28c45ab3978
Kenson Johnson <kenson.johnson@outlook.com>
2023-06-10 14:23:04 -0500

    Created template for chat client ui

--- client/src/pages/dashboard/project/step/task/TaskPage.tsx
+++ client/src/pages/dashboard/project/step/task/TaskPage.tsx
@@ -1,39 +1,43 @@
 import { useParams } from "react-router-dom";
 import { Task, useProjectContext } from "../../../../../utils/GetProjectData";
-import { Card, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import styles from "../../../default/Dashboard.module.css";
+import styles from "./TaskPage.module.css";
+import { useState } from "react";
+import Message from "./Message";
+
+export type ChatMessage = {
+  role: "user" | "assistant";
+  message: string;
+};
 
 export function TaskPage() {
   const { id } = useParams();
   const { project } = useProjectContext();
+  const [conversation, setConversation] = useState<ChatMessage[]>([
+    { role: "assistant", message: "How can I assist?" },
+  ]);
 
   const fetchedTask = project?.taskList.find((task) => task.id === id) as Task;
 
+  const testMessages = [{}];
   return (
-    <>
-      <Container
-        className={`d-flex flex-column my-3 ${styles["dynamic-height"]}`}
+    <Container className="my-4">
+      <NavLink
+        to={`/dashboard/step/${fetchedTask.stepId}`}
+        className="text-success text-decoration-none mt-4 fs-3"
       >
-        <Row className="text-start">
-          <NavLink
-            to={`/dashboard/step/${fetchedTask.stepId}`}
-            className="text-success text-decoration-none fs-3"
-          >
-            <i className="bi bi-chevron-left" /> Step
-          </NavLink>
-        </Row>
-        <h1 className="text-light text-center my-4">{fetchedTask.title}</h1>
-        <Row>
-          <Card>
-            <Card.Header>Task Assistant</Card.Header>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>Card Text</Card.Text>
-            </Card.Body>
-          </Card>
-        </Row>
-      </Container>
-    </>
+        <i className="bi bi-chevron-left" /> Step
+      </NavLink>
+      <h1 className="text-light text-center my-4">{fetchedTask.title}</h1>
+      <div className={styles.box}>
+        <h2 className="border-bottom">Task Assistant</h2>
+        <div className={styles.chat}>
+          <Message role="assistant" setConversation={setConversation} />
+          <Message role="user" setConversation={setConversation} />
+        </div>
+        <div className="border-top">Input</div>
+      </div>
+    </Container>
   );
 }
