fb45a1e0bba1fa1944a9e603ba2d04d0822fd0e2
Kenson Johnson <kenson.johnson@outlook.com>
2023-06-11 16:12:53 -0500

    Finish out homepage

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -5,25 +5,27 @@ export function Main() {
   return (
     <>
       <Featurette
-        heading="Some Content"
-        subHeading="Some more content."
-        content="Imagine some incredible prose here."
+        heading="Intelligent Project Generation"
+        subHeading="All driven by AI"
+        // content="Imagine some incredible prose here."
         reversed={false}
+        imageURL={import.meta.env.BASE_URL + "/project_view.png"}
       />
       <FeaturetteDivider />
       <Featurette
-        heading="Some Content"
-        subHeading="Some more content."
-        content="Imagine some incredible prose here."
+        heading="Built in Task Assistant"
+        subHeading="To maximize productivity"
+        // content="Imagine some incredible prose here."
         reversed={true}
+        imageURL={import.meta.env.BASE_URL + "/task_view.png"}
       />
-      <FeaturetteDivider />
+      {/* <FeaturetteDivider />
       <Featurette
         heading="Some Content"
         subHeading="Some more content."
         content="Imagine some incredible prose here."
         reversed={false}
-      />
+      /> */}
     </>
   );
 }
