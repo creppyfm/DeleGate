6b41dc2ec8e075cc9d89668970cf55e91993417c
Kenson Johnson <kenson.johnson@outlook.com>
2023-06-11 15:48:44 -0500

    Adjusted color scheme on chat box

--- client/src/pages/dashboard/project/step/task/Message.tsx
+++ client/src/pages/dashboard/project/step/task/Message.tsx
@@ -4,7 +4,7 @@ import { ChatMessage } from "./TaskPage";
 export default function Message({ role, content }: ChatMessage) {
   return (
     <Card
-      border={role === "user" ? "success" : "primary"}
+      bg={role === "user" ? "success" : "light"}
       className={`w-50 shadow bg- bg-opacity-10 ${
         role === "user" ? "align-self-end me-3" : ""
       } `}
